4ca9b5c64f3702623bd2622b2a1c4a3b2a4e92e5
sunitsen <69672778+sunitsen@users.noreply.github.com>
2023-01-16 13:50:19 +0600

    message

---
+++ js/index.js
@@ -0,0 +1,207 @@
+//crud application
+var selectedRow = null;
+//showing Alearts
+function showAlert(message, clasName){
+    const div = document.createElement("div");
+    div.classList = `alert alert-${clasName}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div,main);
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+}
+
+//Clear all Fields
+function clearFields() {
+    document.querySelector("#firstName").value ="";
+    document.querySelector("#lastName").value ="";
+    document.querySelector("#rollNo").value ="";
+}
+// Add data
+document.querySelector("#student-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    //get form value
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const rollNo = document.querySelector("#rollNo").value;
+    //validation
+    if(firstName == "" || lastName == "" || rollNo == ""){
+        showAlert("Please fill in all fields", "danger");
+    }else{
+         if(selectedRow == null){
+            const list = document.querySelector("#student-list");
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${rollNo}</td>
+            <td>
+            <a href="#" class="btn btn-warning edit">Edit</a>
+            <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
+            </td>
+            `;
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert("Student Added", "success");
+         }else{
+            selectedRow.children[0].textContent = firstName;
+            selectedRow.children[1].textContent = lastName;
+            selectedRow.children[2].textContent = rollNo;
+            showAlert("Student Info Edited", "info");
+         }
+       clearFields();
+    };
+});
+
+
+//Edit Data
+document.querySelector("#student-list").addEventListener("click", (e) => {
+    target = e.target;
+    if(target.classList.contains("edit")){
+        selectedRow = target.parentElement.parentElement;
+        document.querySelector("#firstName").value = selectedRow.children[0].textContent;
+        document.querySelector("#lastName").value = selectedRow.children[1].textContent;
+        document.querySelector("#rollNo").value = selectedRow.children[2].textContent;
+
+    }
+});
+
+//Delete Data
+document.querySelector("#student-list").addEventListener("click", (e) =>{
+    target = e.target;
+    if(target.classList.contains("delete")){
+        target.parentElement.parentElement.remove();
+        showAlert("Student Data Deleted...!", "danger");
+    }
+});
+
+
+
+
+
+
+
+
+
+//todo application
+
+
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
+
+	const username = localStorage.getItem('username') || '';
+
+	nameInput.value = username;
+
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('username', e.target.value);
+	})
+
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		// Reset the form
+		e.target.reset();
+
+		DisplayTodos()
+	})
+
+	DisplayTodos()
+})
+
+function DisplayTodos () {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
+
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+
+			DisplayTodos()
+
+		})
+
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
+
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+
+	})
+}
